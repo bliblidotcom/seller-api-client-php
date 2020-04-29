6f438ce76b8725a72fd0f31b2133b7a628accb4d
faza.putra <faza.putra@gdn-commerce.com>
2020-04-29 17:07:00 +0700

    * API-638 : Fix indent

--- client/BlibliMerchantClient.php
+++ client/BlibliMerchantClient.php
@@ -21,31 +21,31 @@ class BlibliMerchantClient {
 
 	function invokeGet($url, $params, ApiConfig $request) {
 		$port = 443;
-        $http_method = "GET";
+		$http_method = "GET";
 		return include("invoker/invoke_without_body.php");
 	}
 
 	function invokePost($url, $params, $body, ApiConfig $request) {
 		$port = 443;
-        $http_method = "POST";
+		$http_method = "POST";
 		return include("invoker/invoke_with_body.php");
 	}
 
 	function invokePut($url, $params, $body, ApiConfig $request) {
 		$port = 443;
-        $http_method = "PUT";
+		$http_method = "PUT";
 		return include("invoker/invoke_with_body.php");
 	}
 
 	function invokePatch($url, $params, $body, ApiConfig $request) {
 		$port = 443;
-        $http_method = "PATCH";
+		$http_method = "PATCH";
 		return include("invoker/invoke_with_body.php");
 	}
 
 	function invokeDelete($url, $params, $body, ApiConfig $request) {
 		$port = 443;
-        $http_method = "DELETE";
+		$http_method = "DELETE";
 		return include("invoker/invoke_without_body.php");
 	}
 
